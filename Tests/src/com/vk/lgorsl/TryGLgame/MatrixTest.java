4c318fc9f82c1d0eabe129ef0689e47a2aa993bf
lgor <forxgames007@gmail.com>
2014-11-05 19:42:54 +0300

    tests for vector classes

---
+++ Tests/src/com/vk/lgorsl/TryGLgame/MatrixTest.java
@@ -0,0 +1,11 @@
+package com.vk.lgorsl.TryGLgame;
+
+import android.test.AndroidTestCase;
+
+/**
+ * тестирование моей реализации матриц 3*3 и 4*4
+ *
+ * Created by lgor on 05.11.2014.
+ */
+public class MatrixTest extends AndroidTestCase{
+}
