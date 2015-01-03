be6986c80cac4e4d8207e4a590b3b78dd19b2e89
lgor <forxgames007@gmail.com>
2015-01-03 20:41:10 +0300

    optimizations

--- Cossacks_Renewal/src/com/vk/lgorsl/cossacks/world/realizations/LightView.java
+++ Cossacks_Renewal/src/com/vk/lgorsl/cossacks/world/realizations/LightView.java
@@ -5,13 +5,12 @@ import com.vk.lgorsl.NedoEngine.math.Helper;
 import com.vk.lgorsl.NedoEngine.math.Matrix4_4f;
 import com.vk.lgorsl.NedoEngine.math.Vect2f;
 import com.vk.lgorsl.NedoEngine.math.Vect3f;
-import com.vk.lgorsl.NedoEngine.utils.NedoException;
 import com.vk.lgorsl.cossacks.world.interfaces.ViewBounds;
 import com.vk.lgorsl.cossacks.world.interfaces.iLightView;
 
 /**
  * it creates projection, which covers view of player camera
- *
+ * <p>
  * Created by lgor on 03.01.2015.
  */
 public class LightView implements iLightView {
@@ -24,20 +23,19 @@ public class LightView implements iLightView {
     private final Vect2f direction = new Vect2f();
     private final Vect2f rightDir = new Vect2f();
 
-    private final Vect2f[] verts = new Vect2f[4];
-
     private final Matrix4_4f projection = new Matrix4_4f();
     private final Matrix4_4f another = new Matrix4_4f();
     private final Matrix4_4f multiplier = new Matrix4_4f();
+    private final Matrix4_4f constMatrix = new Matrix4_4f();
 
-    private final Vect3f maxHeight = new Vect3f();
+    private final float maxHeight;
 
     private final float delta;
 
     private final ViewBounds viewBounds = new ViewBounds();
 
-    public LightView(int maxHeight, int bounds){
-        this.maxHeight.set(0, 0, maxHeight);
+    public LightView(int maxHeight, int bounds) {
+        this.maxHeight = maxHeight;
         this.delta = bounds;
     }
 
@@ -48,9 +46,11 @@ public class LightView implements iLightView {
                 0, 0, 0.5f, 0.5f,
                 0, 0, 0, 1);
 
-        for (int i = 0; i < verts.length; i++) {
-            verts[i] = new Vect2f();
-        }
+        constMatrix.set(
+                1, -1, -1, 0,
+                1, 1, -1, 0,
+                1, 1, -1, 2,
+                1, 1, 1, 0);
     }
 
     @Override
@@ -67,68 +67,55 @@ public class LightView implements iLightView {
 
     @Override
     public void setToCover(ViewBounds bounds) {
-        verts[0].set(bounds.leftDown);
-        verts[1].set(bounds.leftUp);
-        verts[2].set(bounds.rightDown);
-        verts[3].set(bounds.rightUp);
 
         rightDir.set(-direction.y, direction.x);
 
-        float maxF = verts[0].dot(direction);
-        float minF = verts[0].dot(direction);
-        float maxR = verts[0].dot(rightDir);
-        float minR = verts[0].dot(rightDir);
+        float maxF = maxDot(direction, bounds) + delta;
+        float minF = minDot(direction, bounds) - delta;
+        float maxR = maxDot(rightDir, bounds) + delta;
+        float minR = minDot(rightDir, bounds) - delta;
 
-        for(int i=1; i< verts.length; i++){
-            maxF = Math.max(maxF, verts[i].dot(direction));
-            minF = Math.min(minF, verts[i].dot(direction));
-            maxR = Math.max(maxR, verts[i].dot(rightDir));
-            minR = Math.min(minR, verts[i].dot(rightDir));
-        }
+        minF -= maxHeight * inclinationCos / inclinationSin;
 
-        maxF += delta;
-        minF -= delta;
-        maxR += delta;
-        minR -= delta;
+        viewBounds.rightUp.x = direction.x*maxF + rightDir.x*maxR;
+        viewBounds.rightUp.y = direction.y*maxF + rightDir.y*maxR;
 
-        minF -= maxHeight.z * inclinationCos / inclinationSin;
+        viewBounds.leftUp.x = direction.x*maxF + rightDir.x*minR;
+        viewBounds.leftUp.y = direction.y*maxF + rightDir.y*minR;
 
-        Vect3f forward = new Vect3f().set(direction.x, direction.y, 0);
-        Vect3f right = new Vect3f().set(rightDir.x, rightDir.y, 0);
+        viewBounds.leftDown.x = direction.x*minF + rightDir.x*minR;
+        viewBounds.leftDown.y = direction.y*minF + rightDir.y*minR;
 
-        Matrix4_4f temp = new Matrix4_4f();
-        Vect3f rightUpFar = new Vect3f().madd(forward, maxF).madd(right, maxR).add(maxHeight);
-        Vect3f leftUpFar = new Vect3f().madd(forward, maxF).madd(right, minR).add(maxHeight);
-        Vect3f leftDownNear = new Vect3f().madd(forward, minF).madd(right, minR);
+        viewBounds.rightDown.x = direction.x*minF + rightDir.x*maxR;
+        viewBounds.rightDown.y = direction.y*minF + rightDir.y*maxR;
 
-        Vect3f rightNear = new Vect3f().madd(forward, minF).madd(right, maxR).add(maxHeight);
+        projection.setColumn(0, viewBounds.rightUp.x, viewBounds.rightUp.y, maxHeight, 1);
+        projection.setColumn(1, viewBounds.leftUp.x, viewBounds.leftUp.y, maxHeight, 1);
+        projection.setColumn(2, viewBounds.leftDown.x, viewBounds.leftDown.y, 0, 1);
+        projection.setColumn(3, direction.x*inclinationCos*(maxF-minF), direction.y*inclinationCos*(maxF-minF), -inclinationSin*(maxF-minF), 0);
 
-        viewBounds.rightUp.set(rightUpFar.x, rightUpFar.y);
-        viewBounds.leftUp.set(leftUpFar.x, leftUpFar.y);
-        viewBounds.leftDown.set(leftDownNear.x, leftDownNear.y);
-        viewBounds.rightDown.set(rightNear.x, rightNear.y);
+        projection.invert();
 
-        Vect3f xz = new Vect3f().madd(viewDirection(), maxF-minF);
-
-        temp.setColumn(0, rightUpFar, 1);
-        temp.setColumn(1, leftUpFar, 1);
-        temp.setColumn(2, leftDownNear, 1);
-        temp.setColumn(3, xz, 0);   //null is feature
-
-        if(!temp.invert()){
-            throw new NedoException("Matrix wasn't inverted!");
-        }
-
-        Matrix4_4f temp2 = new Matrix4_4f().set(
-                1, -1, -1, 0,
-                1,  1, -1, 0,
-                1,  1, -1, 2,
-                1,  1,  1, 0);
-
-
-        projection.multiplication(temp2, temp);
-
+        projection.multiplication(constMatrix, projection);
         another.multiplication(multiplier, projection);
+    }
+
+    private float maxDot(Vect2f r, ViewBounds bounds){
+        float dot, max;
+        max = bounds.leftDown.dot(r);
+        if ((dot = bounds.leftUp.dot(r)) > max) max = dot;
+        if ((dot = bounds.rightDown.dot(r)) > max) max = dot;
+        if ((dot = bounds.rightUp.dot(r)) > max) max = dot;
+        return max;
+    }
+
+    private float minDot(Vect2f r, ViewBounds bounds){
+        float dot, min;
+        min = bounds.leftDown.dot(r);
+        if ((dot = bounds.leftUp.dot(r)) < min) min = dot;
+        if ((dot = bounds.rightDown.dot(r)) < min) min = dot;
+        if ((dot = bounds.rightUp.dot(r)) < min) min = dot;
+        return min;
     }
 
     @Override
