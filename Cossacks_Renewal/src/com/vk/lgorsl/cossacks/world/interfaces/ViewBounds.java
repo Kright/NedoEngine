592ee6b4cf7b39bd9c7b45c37679f286a92d2ae0
lgor <forxgames007@gmail.com>
2015-01-06 14:59:36 +0300

    added working tests module, fixed ViewBounds method

--- Cossacks_Renewal/src/com/vk/lgorsl/cossacks/world/interfaces/ViewBounds.java
+++ Cossacks_Renewal/src/com/vk/lgorsl/cossacks/world/interfaces/ViewBounds.java
@@ -4,7 +4,7 @@ import com.vk.lgorsl.NedoEngine.math.*;
 
 /**
  * bound of camera's view on 2d map
- *
+ * <p>
  * Created by lgor on 28.12.2014.
  */
 public class ViewBounds {
@@ -20,20 +20,20 @@ public class ViewBounds {
         leftDown = new Vect2f();
         rightUp = new Vect2f();
         rightDown = new Vect2f();
-        for(int i=0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             normals[i] = new Point2i();
             verts[i] = new Point2i();
         }
     }
 
-    public void getAABB(Rectangle2i result){
-        result.xMax = (int)Helper.max(leftUp.x, leftDown.x, rightUp.x, rightDown.x);
-        result.xMin = (int)Helper.min(leftUp.x, leftDown.x, rightUp.x, rightDown.x);
-        result.yMax = (int)Helper.max(leftUp.y, leftDown.y, rightUp.y, rightDown.y);
-        result.yMin = (int)Helper.min(leftUp.y, leftDown.y, rightUp.y, rightDown.y);
+    public void getAABB(Rectangle2i result) {
+        result.xMax = (int) Helper.max(leftUp.x, leftDown.x, rightUp.x, rightDown.x);
+        result.xMin = (int) Helper.min(leftUp.x, leftDown.x, rightUp.x, rightDown.x);
+        result.yMax = (int) Helper.max(leftUp.y, leftDown.y, rightUp.y, rightDown.y);
+        result.yMin = (int) Helper.min(leftUp.y, leftDown.y, rightUp.y, rightDown.y);
     }
 
-    public void update(){
+    public void update() {
         Vect2f center = new Vect2f();
         center.set(leftUp).add(leftDown).add(rightUp).add(rightDown).mul(0.25f);
 
@@ -42,40 +42,40 @@ public class ViewBounds {
         calculate(2, rightUp, leftUp, center);
         calculate(3, leftUp, leftDown, center);
 
-        verts[0].set((int)leftDown.x, (int)leftDown.y);
-        verts[1].set((int)leftUp.x, (int)leftUp.y);
-        verts[2].set((int)rightDown.x, (int)rightDown.y);
-        verts[3].set((int)rightUp.x, (int)rightUp.y);
+        verts[0].set((int) leftDown.x, (int) leftDown.y);
+        verts[1].set((int) leftUp.x, (int) leftUp.y);
+        verts[2].set((int) rightDown.x, (int) rightDown.y);
+        verts[3].set((int) rightUp.x, (int) rightUp.y);
     }
 
-    private void calculate(int number, Vect2f v1, Vect2f v2, Vect2f center){
-        int nx = (int)(v1.y- v2.y);
-        int ny = (int)(v2.x - v1.x);
-        int dc = (int)(nx*center.x + ny*center.y);
-        int d = (int)(nx * v1.x + ny * v2.x);
+    private void calculate(int number, Vect2f v1, Vect2f v2, Vect2f center) {
+        int nx = (int) (v1.y - v2.y);
+        int ny = (int) (v2.x - v1.x);
+        int dc = (int) (nx * center.x + ny * center.y);
+        int d = (int) (nx * v1.x + ny * v1.y);
         if (dc < d) {
             nx *= -1;
             ny *= -1;
-            d *=-1;
+            d *= -1;
         }
-        assert(dc > d);
+        assert (dc > d);
         normals[number].x = nx;
         normals[number].y = ny;
         pr[number] = d;
     }
 
-    public boolean contains(iPoint2i point){
+    public boolean contains(iPoint2i point) {
         int px = point.x();
         int py = point.y();
-        for(int i=0; i<4; i++){
-            if (px * normals[i].x + py * normals[i].y < pr[i]){
+        for (int i = 0; i < 4; i++) {
+            if (px * normals[i].x + py * normals[i].y < pr[i]) {
                 return false;
             }
         }
         return true;
     }
 
-    public boolean intersects(iRectangle2i rectangle){
+    public boolean intersects(iRectangle2i rectangle) {
         int xMin = rectangle.xMin();
         int xMax = rectangle.xMax();
         int yMin = rectangle.yMin();
@@ -84,7 +84,7 @@ public class ViewBounds {
         boolean smallerX = true;
         boolean biggerY = true;
         boolean smallerY = true;
-        for(int i=0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             if (verts[i].x <= xMax) biggerX = false;
             if (verts[i].x >= xMin) smallerX = false;
             if (verts[i].y <= yMax) biggerY = false;
