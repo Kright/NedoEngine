6027fea5dff3b1505360f02f9ce899faeaf831c2
lgor <forxgames007@gmail.com>
2015-01-13 23:17:06 +0300

    new BufferGeneration uses WeakReference and doesn't spend a lot of memory

--- Cossacks_Renewal/src/com/vk/lgorsl/cossacks/graphics/LandscapeChunk.java
+++ Cossacks_Renewal/src/com/vk/lgorsl/cossacks/graphics/LandscapeChunk.java
@@ -7,6 +7,7 @@ import com.vk.lgorsl.NedoEngine.math.iRectangle2i;
 import com.vk.lgorsl.NedoEngine.openGL.GLHelper;
 import com.vk.lgorsl.cossacks.world.interfaces.iLandscapeMap;
 
+import java.lang.ref.WeakReference;
 import java.nio.FloatBuffer;
 import java.nio.ShortBuffer;
 
@@ -16,6 +17,8 @@ import java.nio.ShortBuffer;
  * Created by lgor on 06.01.2015.
  */
 public class LandscapeChunk {
+
+    private static WeakReference<float[]> tempArray = new WeakReference<>(new float[16]);
 
     public final Rectangle2i area = new Rectangle2i();
     public int width, height;
@@ -34,12 +37,18 @@ public class LandscapeChunk {
         width = area.width() / cellSize + 1;
         height = area.height() / cellSize + 1;
 
-        makeVerticesAndNormals(map, cellSize);
-        if (generateIndices){
+        makeVerticesAndNormalsWithWeak(map, cellSize);
+        if (generateIndices) {
             indices = makeIndices(width, height);
         }
     }
 
+    /**
+     * old, more simple realization of buffers creating
+     * function create new temp arrays for each call
+     * It is not used because of garbage collector
+     */
+    @Deprecated
     private void makeVerticesAndNormals(iLandscapeMap map, int cellSize) {
         float[] verts = new float[width * height * 3];
         float[] norms = new float[width * height * 3];
@@ -59,6 +68,37 @@ public class LandscapeChunk {
         }
         vertices = GLHelper.make(verts);
         normals = GLHelper.make(norms);
+    }
+
+    private void makeVerticesAndNormalsWithWeak(iLandscapeMap map, int cellSize) {
+        final int requiredLength = width * height * 6;
+        float[] tempArr = tempArray.get();
+        if (tempArr == null || tempArr.length < requiredLength) {
+            tempArr = new float[requiredLength];
+            tempArray = new WeakReference<>(tempArr);
+        }
+        int delta = width * height * 3;
+        int pos = 0;
+        Point2i p = new Point2i();
+        Vect3f n = new Vect3f();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                p.x = i * cellSize + area.xMin();
+                p.y = j * cellSize + area.yMin();
+                map.getNormal(p, n);
+                n.putIntoArray(tempArr, pos + delta);
+                tempArr[pos++] = p.x;
+                tempArr[pos++] = p.y;
+                tempArr[pos++] = map.getHeight(p);
+            }
+        }
+        vertices = GLHelper.makeFloatBuffer(width * height * 3);
+        vertices.put(tempArr, 0, width * height * 3);
+        vertices.position(0);
+
+        normals = GLHelper.makeFloatBuffer(width * height * 3);
+        normals.put(tempArr, delta, width * height * 3);
+        normals.position(0);
     }
 
     public static ShortBuffer makeIndices(int width, int height) {
